e1d1921b184b779be1c568f0274a492b56d09904
Chris Williams <chris.williams@sonomarin.net>
2022-11-25 19:43:02 -0600

    Section 2.9

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Base CI4</title>
+    <title>Udemy CI4 TaskApp</title>
   </head>
   <body>
     <h1>Base Development Environment</h1>
