32e9854627adc0338146c4bf79b2bc170a02cf4f
Chris Williams <chris.williams@sonomarin.net>
2022-11-25 23:00:10 -0600

    Section 4.25

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -4,43 +4,54 @@ namespace App\Controllers;
 
 class Tasks extends BaseController
 {
-    public function index()
-    {
+	public function index()
+	{
         $model = new \App\Models\TaskModel;
         $data = $model->findAll();
-
-        return view("Tasks/index", [
-            'tasks' => $data
-        ]);
-    }
-
-    public function show($id)
+		
+		return view("Tasks/index", ['tasks' => $data]);
+	}
+	
+	public function show($id)
     {
         $model = new \App\Models\TaskModel;
-
+		
         $task = $model->find($id);
-
-        return view("Tasks/show",[
+		
+		return view('Tasks/show', [
             'task' => $task
         ]);
-    }
-
-    public function new()
-    {
-        return view('Tasks/new');
-    }
-
-    public function create()
-    {
+	}
+	
+	public function new()
+	{
+		return view('Tasks/new');
+	}
+	
+	public function create()
+	{
         $model = new \App\Models\TaskModel;
-
-        $model->insert([
-            'description' => $this->request->getPost('description')
-        ]);
-
-        dd($model->insertID);
-        
-
-    }
-
+		
+		$result = $model->insert([
+			'description' => $this->request->getPost("description")
+		]);
+		
+        if ($result === false) {
+			
+            dd($model->errors());
+		
+        } else {
+		
+			dd($result);
+			
+		}
+	}
 }
+
+
+
+
+
+
+
+
