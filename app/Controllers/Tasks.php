c73a5d610f807b4946bcb7fa433f2670c7a1083b
Chris Williams <chris.williams@sonomarin.net>
2022-11-25 21:52:05 -0600

    Section 3.19

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -6,10 +6,9 @@ class Tasks extends BaseController
 {
     public function index()
     {
-        $data = [
-            ['id' => 1, 'description' => 'First Task'],
-            ['id' => 2, 'description' => 'Second Task']
-        ];
+        $model = new \App\Models\TaskModel;
+        $data = $model->findAll();
+
         return view("Tasks/index", ['tasks' => $data]);
     }
 }
