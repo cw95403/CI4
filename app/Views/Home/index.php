42fc70c60d38855605bec8142531c949db716f72
Chris Williams <chris.williams@sonomarin.net>
2022-11-26 22:25:08 -0600

    Section 8.60

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -6,20 +6,4 @@
 <? $this->section("content") ?>
    <h1>Welcome Home!</h1>
 
-   <a href="<?= site_url("/signup") ?>">Sign Up</a>   
-
-   <?php if(current_user()): ?>
-
-      <p>User is logged in</p>
-      <p>Hello <?= esc(current_user()->name) ?></p>
-
-      <a href="<?= site_url("/logout") ?>">Logout</a>
-
-   <?php else: ?>
-
-      <p>User is not logged in</p>
-      <a href="<?= site_url("/login") ?>">Login</a>
-
-   <?php endif; ?>
-
 <? $this->endSection() ?>
--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -10,7 +10,21 @@
 
     <title><?= $this->renderSection("title") ?></title>
   </head>
-  <body>
+  <body>  
+
+  <a href="<?= site_url("/") ?>">Home</a>
+
+  <?php if(current_user()): ?>
+
+    <p>Hello <?= esc(current_user()->name) ?></p>
+    <a href="<?= site_url("/tasks") ?>">Tasks</a>
+    <a href="<?= site_url("/logout") ?>">Logout</a>
+
+  <?php else: ?>
+    <a href="<?= site_url("/login") ?>">Login</a>
+    <a href="<?= site_url("/signup") ?>">Sign Up</a> 
+
+  <?php endif; ?>
 
   <?php if (session()->has('warning')): ?>
     <div class="warning">
