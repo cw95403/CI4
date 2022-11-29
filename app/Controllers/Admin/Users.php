d32d4d429784d7fcaf05672532e65ddef26dedcf
Chris Williams <chris.williams@sonomarin.net>
2022-11-28 18:46:06 -0600

    Section 10.71

--- app/Controllers/Admin/Users.php
+++ app/Controllers/Admin/Users.php
@@ -4,9 +4,21 @@ namespace App\Controllers\Admin;
 
 class Users extends \App\Controllers\BaseController
 {
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new \App\Models\UserModel;
+    }
     public function index()
     {
-        return view('Admin/Users/index');
+       $users = $this->model->orderBy('id')
+                    ->paginate(5);
+
+        return view('Admin/Users/index', [
+            'users' => $users,
+            'pager' => $this->model->pager
+        ]);
     }
 
 }
--- app/Views/Admin/Users/index.php
+++ app/Views/Admin/Users/index.php
@@ -1,1 +1,40 @@
-This is Admin/Users/Index.php
+<?= $this->extend("layouts/default") ?>
+
+<? $this->section("title") ?>TaskApp-Users<? $this->endSection() ?>
+
+<? $this->section("content") ?>
+   <h1>Users</h1>
+   
+   <a href="<?= site_url("/admin/users/new") ?>">New User</a>
+
+   <?php if ($users): ?>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($users as $user): ?>
+                
+                <tr>
+                    <td><a href="<?= site_url("/admin/users/show/" . $user->id) ?>"><?= esc($user->name) ?></a></td>
+                    <td><?= esc($user->email) ?></td>
+                    <td><?= $user->created_at ?></td>
+                </tr>
+
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+      <?= $pager->links() ?>
+
+   <?php else: ?>
+      <p>no users...</p>
+
+   <?php endif ?>
+
+<? $this->endSection() ?>
